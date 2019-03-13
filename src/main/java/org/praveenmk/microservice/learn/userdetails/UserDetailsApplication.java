019c339a584bfdfc72442facaedc7b4c141b94d2
Praveen MK <praveen.mk@outlook.com>
2019-03-14 01:07:24 +0530

    enable Eureka

--- src/main/java/org/praveenmk/microservice/learn/userdetails/UserDetailsApplication.java
+++ src/main/java/org/praveenmk/microservice/learn/userdetails/UserDetailsApplication.java
@@ -5,9 +5,11 @@ import org.praveenmk.microservice.learn.userdetails.repository.UserDetailsReposi
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserDetailsApplication {
 
 	public static void main(String[] args) {
